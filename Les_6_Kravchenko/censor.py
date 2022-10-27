6a360cc9f7b7bdf3e2fc86c8edbd9ef4c3e04a30
Daramos <YuriyDaramos@users.noreply.github.com>
2022-10-27 13:51:48 +0300

    changed cycle "for" to "map()" in lower_blacklist generation

--- Les_6_Kravchenko/censor.py
+++ Les_6_Kravchenko/censor.py
@@ -1,10 +1,10 @@
-text_ = "You motherfucker, come on, you little ass... fuck with me, eh?! You fucking little asshole, dickhead, " \
-        "cocksucker... You fuckin’ — come on, come fuck with me! I’ll get your ass, you jerk! Oh, you fuckhead, " \
-        "motherfucker! Fuck all you and your family! Come on, you cocksucker, slime bucket, shitface, turdball! Come " \
-        "on, you scum sucker, you fucking with me?! Come on, you asshole!"
+badword_text = "You motherfucker, come on, you little ass... fuck with me, eh?! You fucking little asshole, " \
+               "dickhead, cocksucker... You fuckin’ — come on, come fuck with me! I’ll get your ass, you jerk! Oh, " \
+               "you fuckhead, motherfucker! Fuck all you and your family! Come on, you cocksucker, slime bucket, " \
+               "shitface, turdball! Come on, you scum sucker, you fucking with me?! Come on, you asshole! "
 
-blacklist_ = ["Motherfucker", "ass", "fuck", "fucking", "asshole", "dickhead", "cocksucker", "fuckin’", "jerk",
-              "fuckhead", "shitface", "turdball", "Sucker"]
+badword_blacklist = ["Motherfucker", "ass", "fuck", "fucking", "asshole", "dickhead", "cocksucker", "fuckin", "jerk",
+                     "fuckhead", "shitface", "turdball", "Sucker"]
 
 
 def censor_text(text, blacklist):
@@ -19,27 +19,30 @@ def censor_text(text, blacklist):
     :return: Зацензуренный текст
     :rtype: str
     """
-    lower_blacklist = []
-    word_list = text.split(" ")
-    index = 0  # for go to next word
 
-    for word in blacklist:
-        lower_blacklist.append(word.lower())
+    word_list = text.split(" ")     # Список слов в тексте
+    index = 0  # Счётчик слов, нужен для итерации слов
 
+    lower_blacklist = list(map(str.lower, blacklist))
+
+    # Поиск и обработка "плохих" слов
     for word in word_list:
-        word_stripped = word.rstrip(",.!?'")
+        word_stripped = word.rstrip(",.!?'’")
         if word_stripped.lower() in lower_blacklist:
             if len(word_stripped) != len(word):
+                # spm -- кол-во потерянных знаков препинания после "раздевания" слова
                 spm = len(word_stripped) - len(word)  # spm -- Stripped Punctuation Marks
             else:
                 spm = 0
-            word_list[index] = word[0] + "*" * (len(word_stripped) - 1) + word[-1] * (spm * -1)
+            # замена определенных слов в тексте (по факту замена элементов списка)
+            # слово в тексте = первый символ слова + кол-во "*" равное кол-ву оставшихся символов + знаки пунктуации
+            word_list[index] = word[0] + ("*" * (len(word_stripped) - 1)) + (word[-1] * (spm * -1))
         index += 1
     return " ".join(word_list)
 
 
 print("---------------------------------")
-print(text_)
+print(badword_text)
 print("---------------------------------")
-print(censor_text(text_, blacklist_))
+print(censor_text(badword_text, badword_blacklist))
 print("---------------------------------")
