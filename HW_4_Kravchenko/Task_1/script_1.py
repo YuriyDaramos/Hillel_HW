105b8cc942cd40090eecbd44edea3aed5e15b574
Daramos <YuriyDaramos@users.noreply.github.com>
2022-11-22 22:14:12 +0200

    stage 2 is complete

--- HW_4_Kravchenko/Task_1/script_1.py
+++ HW_4_Kravchenko/Task_1/script_1.py
@@ -1,7 +1,24 @@
 import json
-from HW_4_Kravchenko.Task_1.exceptions import NotFound
+from exceptions import NotFound, NoAccess
 
 
+def user_id(get_company_city):
+    def wrapper(user_id: str, company_id: str):
+        with open("users_db.json") as f:
+            users_db = json.load(f)
+        # print(users_db.get(user_id))
+        # print(int(company_id))
+        # print(users_db.get(user_id).get('companies'))
+        if users_db.get(user_id) is None:
+            raise NotFound(f"User with ID '{user_id}' does not exist")
+        elif int(company_id) in users_db.get(user_id).get('companies'):
+            return get_company_city(company_id)
+        else:
+            raise NoAccess(f"User with ID '{user_id}' does not have enough permissions")
+    return wrapper
+
+
+@user_id
 def get_company_city(company_id: str) -> str:
     """Возвращает город регистрации компании по её ID.
 
@@ -22,5 +39,5 @@ def get_company_city(company_id: str) -> str:
 
 
 if __name__ == "__main__":
-    print(get_company_city("1"))
+    print(get_company_city(user_id="1", company_id="1"))
     # print(get_company_city("test"))
